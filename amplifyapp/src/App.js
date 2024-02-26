24b32f8bac2b200730bb7753e307501236eae117
Olivia Irvine <olivia@olivias-air-9.lan>
2024-02-25 19:48:19 -0500

    commit two

---
+++ amplifyapp/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        CS 230L
+      </h1>
+      <h2>
+        Section - 002
+      </h2>
+      <p>
+        WVU ID: 800396819
+      </p>
+      <p>
+        Hi I am Olivia Irvine
+      </p>
+    </div>
+  );
+}
+
+export default App;
